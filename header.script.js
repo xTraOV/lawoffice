9f6def4895fb2b00e9b57a273e008b2856799156
xTraOV <vahakobalava@gmail.com>
2022-02-24 16:28:28 +0400

    some organisation

--- header.script.js
+++ header.script.js
@@ -3,14 +3,22 @@ const navCloseButton = document.querySelector(".hamburger-menu-exit");
 
 const navList = document.querySelector(".navigation-list");
 
+const body = document.querySelector("body");
+
 navOpenButton.addEventListener("click", () => {
     navOpenButton.classList.add("hidden");
     navCloseButton.classList.remove("hidden");
     navList.classList.add("mobile-open");
+    setTimeout(() => {
+        navList.classList.add("transition");
+        body.classList.add("menu-open");
+    }, 1);
 });
 
 navCloseButton.addEventListener("click", () => {
     navOpenButton.classList.remove("hidden");
     navCloseButton.classList.add("hidden");
     navList.classList.remove("mobile-open");
+    navList.classList.remove("transition");
+    body.classList.remove("menu-open");
 });
