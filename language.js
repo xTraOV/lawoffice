89e73b035bdbb153a453d3c17c241000fffb6b24
xtraov <vazhakobalava@gmail.com>
2022-03-30 11:46:04 +0400

    qwe

--- language.js
+++ language.js
@@ -1,72 +1,75 @@
-let currentLanguage = localStorage.getItem('currentLanguage');
+let currentLanguage = localStorage.getItem("currentLanguage");
 
-const languages = 
-{    
-    "english" : [
-        {"className":"services", "innerText": "services"},
-        {"className":"news","innerText":"news"},
-        {"className":"blog","innerText":"blog"},
-        {"className":"contact","innerText":"contact"},
-        {"className":"lawier-name","innerText":"GIORGI KORASHVILI"},
-        {"className":"article-content","innerText":`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+const languages = {
+    english: [
+        { className: "services", innerText: "services" },
+        { className: "news", innerText: "news" },
+        { className: "blog", innerText: "blog" },
+        { className: "contact", innerText: "contact" },
+        { className: "language-indicator", innerText: "GE" },
+        { className: "lawier-name", innerText: "GIORGI KORASHVILI" },
+        {
+            className: "article-content",
+            innerText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        },
     ],
-    "georgian" : [
-        {"className":"services", "innerText": "სერვისები"},
-        {"className":"news","innerText":"სიახლეები"},
-        {"className":"blog","innerText":"ბლოგი"},
-        {"className":"contact","innerText":"კონტაქტი"},
-        {"className":"lawier-name","innerText":"გიორგი კორაშვილი"},
-        {"className":"article-content","innerText":"ლორემ იპსუმ დოლორ სიტ ამეტ, ცონსეცტეტურ ადიპისცინგ ტემპორ ინციდიდნუტ უტ ლაბორე ეტ დოლორე მაგნა ალიქუა. უტ ენიმ ად მინიმ ვენია"}
-    ]
-}
+    georgian: [
+        { className: "services", innerText: "სერვისები" },
+        { className: "news", innerText: "სიახლეები" },
+        { className: "blog", innerText: "ბლოგი" },
+        { className: "contact", innerText: "კონტაქტი" },
+        { className: "language-indicator", innerText: "EN" },
+        { className: "lawier-name", innerText: "გიორგი კორაშვილი" },
+        {
+            className: "article-content",
+            innerText:
+                "ლორემ იპსუმ დოლორ სიტ ამეტ, ცონსეცტეტურ ადიპისცინგ ტემპორ ინციდიდნუტ უტ ლაბორე ეტ დოლორე მაგნა ალიქუა. უტ ენიმ ად მინიმ ვენია",
+        },
+    ],
+};
 
 const button = document.querySelector(".change_language");
-const switchLanguage = () => 
-{
-    if(currentLanguage=='georgian'){
-        window.localStorage.setItem('currentLanguage','english');
-        currentLanguage = localStorage.getItem('currentLanguage');
-        for(const item of languages.english){
+const switchLanguage = () => {
+    if (currentLanguage == "georgian") {
+        window.localStorage.setItem("currentLanguage", "english");
+        currentLanguage = localStorage.getItem("currentLanguage");
+        for (const item of languages.english) {
+            let toAdd = document.createTextNode(`${item.innerText}`);
+            let addTo = document.querySelector(`.${item.className}`);
+            addTo.firstChild && addTo.removeChild(addTo.firstChild);
+            addTo.appendChild(toAdd);
+        }
+    } else {
+        window.localStorage.setItem("currentLanguage", "georgian");
+        currentLanguage = localStorage.getItem("currentLanguage");
+        for (const item of languages.georgian) {
             let toAdd = document.createTextNode(`${item.innerText}`);
             let addTo = document.querySelector(`.${item.className}`);
             addTo.firstChild && addTo.removeChild(addTo.firstChild);
             addTo.appendChild(toAdd);
         }
     }
-    else{
-        window.localStorage.setItem('currentLanguage','georgian');
-        currentLanguage = localStorage.getItem('currentLanguage');
-        for(const item of languages.georgian){
-            let toAdd = document.createTextNode(`${item.innerText}`);
-            let addTo = document.querySelector(`.${item.className}`);
-            addTo.firstChild && addTo.removeChild(addTo.firstChild);
-            addTo.appendChild(toAdd);
-        }
-    }
-}
+};
 
-const loadLanguage = () =>
-{
-    if(currentLanguage=='english'){
-        for(const item of languages.english){
+const loadLanguage = () => {
+    if (currentLanguage == "english") {
+        for (const item of languages.english) {
+            let toAdd = document.createTextNode(`${item.innerText}`);
+            let addTo = document.querySelector(`.${item.className}`);
+            addTo.appendChild(toAdd);
+        }
+    } else {
+        for (const item of languages.georgian) {
             let toAdd = document.createTextNode(`${item.innerText}`);
             let addTo = document.querySelector(`.${item.className}`);
             addTo.appendChild(toAdd);
         }
     }
-    else
-    {
-        for(const item of languages.georgian){
-            let toAdd = document.createTextNode(`${item.innerText}`);
-            let addTo = document.querySelector(`.${item.className}`);
-            addTo.appendChild(toAdd);
-        }
-    }
-}
+};
 
 loadLanguage();
-button.addEventListener('click', () => switchLanguage());
+button.addEventListener("click", () => switchLanguage());
